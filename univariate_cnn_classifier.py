7f3a4fde6f19ece1e64b996340746535a3a33bcb
lws803 <lws803@gmail.com>
2019-07-11 23:08:10 +0800

    Made some changes to univariate cnn classifier

--- univariate_cnn_classifier.py
+++ univariate_cnn_classifier.py
@@ -6,11 +6,14 @@ import torch.optim as optim
 
 NUM_CLASSES = 4
 WINDOW_SIZE = 3
-GENERALISE = False
+GENERALISE = True
+NUM_CONV_LAYERS = 2
 
-X = np.array([[10, 20, 30], [70, 60, 50], [70, 70, 70], [10, 30, 10]])  # Time series
-y = np.array([0, 1, 2, 3])  # Classifications
+X = np.array([[10, 20, 30], [70, 60, 50], [70, 70, 70], [10, 30, 10], [20, 20, 20]])  # Time series
+y = np.array([0, 1, 2, 3, 2])  # Classifications
 
+# TODO: Consider using padding instead. Always set WINDOW_SIZE to the largest window
+# Smaller dataset will be padded instead
 
 class Flatten(nn.Module):
     def forward(self, input):
@@ -21,26 +24,28 @@ class Network (nn.Module):
         super(Network, self).__init__()
         # Observations: Increasing the number of hidden layers post-conv increases the accuracy in prediction
         # in_channels determine the number of data points per sequence
-        # TODO: Tune and find the best model
         self.conv = nn.Conv1d(in_channels=WINDOW_SIZE, out_channels=10, kernel_size=2, padding=(2 // 2))
         self.conv2 = nn.Conv1d(in_channels=10, out_channels=10, kernel_size=2, padding=(2 // 2))
+        self.bn1 = nn.BatchNorm1d(num_features=10)  # TODO: See if normalisation helps
+
         self.dropout = nn.Dropout(p=0.5)
-        self.pool = nn.MaxPool1d(2)
+        self.pool = nn.MaxPool1d(NUM_CONV_LAYERS)
         self.flatten = Flatten()
         self.linear1 = nn.Linear(10, 50)
         self.output = nn.Linear(50, NUM_CLASSES)
 
     def forward(self, x):
         x = self.conv(x)
+        # x = self.bn1(x)
         x = F.relu(x)
         x = self.conv2(x)
+        # x = self.bn1(x)
         x = F.relu(x)
+
         if GENERALISE:
             x = self.dropout(x)  # For added benefit of generalisation but will make training performance worse
         x = self.pool(x)
         x = self.flatten(x)
-        # TODO: Add more convolutional layers here and flatten them as well then concatenate into a giant network
-        # So we get a larger FC here
         x = F.relu(self.linear1(x))
         x = F.softmax(self.output(x))
         return x
