cb05068acfaa2d7063c37096cad101690aa2ffed
lws803 <lws803@gmail.com>
2019-07-11 09:02:59 +0800

    Abstracted some variables and magic numbers

--- univariate_cnn_classifier.py
+++ univariate_cnn_classifier.py
@@ -4,8 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-X = np.array([[10,20,30], [70,60,50], [70, 70, 70]])
-y = np.array([0,1,2])
+NUM_CLASSES = 4
+WINDOW_SIZE = 3
+
+X = np.array([[10,20,30], [70,60,50], [70, 70, 70], [10,30,10]]) # Time series
+y = np.array([0,1,2,3]) # Classifications
 
 
 class Flatten(nn.Module):
@@ -18,13 +21,13 @@ class Network (nn.Module):
         # Observations: Increasing the number of hidden layers post-conv increases the accuracy in prediction
         # in_channels determine the number of data points per sequence
         # TODO: Tune and find the best model
-        self.conv = nn.Conv1d(in_channels=3, out_channels=10, kernel_size=2,padding=(2 // 2))
+        self.conv = nn.Conv1d(in_channels=WINDOW_SIZE, out_channels=10, kernel_size=2,padding=(2 // 2))
         self.conv2 = nn.Conv1d(in_channels=10, out_channels=10, kernel_size=2,padding=(2 // 2))
         self.dropout = nn.Dropout(p=0.5)
         self.pool = nn.MaxPool1d(2)
         self.flatten = Flatten()
         self.linear1 = nn.Linear(10, 50)
-        self.output = nn.Linear(50, 3)
+        self.output = nn.Linear(50, NUM_CLASSES)
 
     def forward (self, x):
         x = self.conv(x)
@@ -84,6 +87,14 @@ conf, predicted = torch.max(outputs.data, 1)
 print (predicted[0], conf)
 
 x_input = np.array([60, 60, 60])
+x_input = x_input.reshape((1, 3, 1))
+test_input = torch.tensor(x_input)
+test_input = test_input.type(torch.FloatTensor)
+outputs = net(test_input)
+conf, predicted = torch.max(outputs.data, 1)
+print (predicted[0], conf)
+
+x_input = np.array([20, 35, 20])
 x_input = x_input.reshape((1, 3, 1))
 test_input = torch.tensor(x_input)
 test_input = test_input.type(torch.FloatTensor)
