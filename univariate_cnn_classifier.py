473e15f58d8ee6e776f37eb3df09947a5fc1a1f1
lws803 <lws803@gmail.com>
2019-07-11 09:17:19 +0800

    refactored code

--- univariate_cnn_classifier.py
+++ univariate_cnn_classifier.py
@@ -6,9 +6,10 @@ import torch.optim as optim
 
 NUM_CLASSES = 4
 WINDOW_SIZE = 3
+GENERALISE = False
 
-X = np.array([[10,20,30], [70,60,50], [70, 70, 70], [10,30,10]]) # Time series
-y = np.array([0,1,2,3]) # Classifications
+X = np.array([[10, 20, 30], [70, 60, 50], [70, 70, 70], [10, 30, 10]])  # Time series
+y = np.array([0, 1, 2, 3])  # Classifications
 
 
 class Flatten(nn.Module):
@@ -16,26 +17,26 @@ class Flatten(nn.Module):
         return input.view(input.size(0), -1)
 
 class Network (nn.Module):
-    def __init__ (self):
+    def __init__(self):
         super(Network, self).__init__()
         # Observations: Increasing the number of hidden layers post-conv increases the accuracy in prediction
         # in_channels determine the number of data points per sequence
         # TODO: Tune and find the best model
-        self.conv = nn.Conv1d(in_channels=WINDOW_SIZE, out_channels=10, kernel_size=2,padding=(2 // 2))
-        self.conv2 = nn.Conv1d(in_channels=10, out_channels=10, kernel_size=2,padding=(2 // 2))
+        self.conv = nn.Conv1d(in_channels=WINDOW_SIZE, out_channels=10, kernel_size=2, padding=(2 // 2))
+        self.conv2 = nn.Conv1d(in_channels=10, out_channels=10, kernel_size=2, padding=(2 // 2))
         self.dropout = nn.Dropout(p=0.5)
         self.pool = nn.MaxPool1d(2)
         self.flatten = Flatten()
         self.linear1 = nn.Linear(10, 50)
         self.output = nn.Linear(50, NUM_CLASSES)
 
-    def forward (self, x):
+    def forward(self, x):
         x = self.conv(x)
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
-        # TODO: Test out the dropout layer, see if its preferred to prevent overfitting
-        x = self.dropout(x)
+        if GENERALISE:
+            x = self.dropout(x)  # For added benefit of generalisation but will make training performance worse
         x = self.pool(x)
         x = self.flatten(x)
         # TODO: Add more convolutional layers here and flatten them as well then concatenate into a giant network
@@ -43,6 +44,7 @@ class Network (nn.Module):
         x = F.relu(self.linear1(x))
         x = F.softmax(self.output(x))
         return x
+
 
 net = Network()
 criterion = nn.CrossEntropyLoss()
@@ -58,47 +60,30 @@ for epoch in range(1000):  # loop over the dataset multiple times
 
     optimizer.zero_grad()
     outputs = net(inputs)
-     
+
     loss = criterion(outputs, labels)
     loss.backward()
     optimizer.step()
 
     running_loss += loss.item()
     print('[%d] loss: %.3f' %
-        (epoch + 1, running_loss / (epoch+1)))
+        (epoch + 1, running_loss / (epoch + 1)))
 
-print ("====================Finished training=================")
+print("====================Finished training=================")
 
+evaluation_set = [
+    [15, 25, 35],
+    [45, 70, 80],
+    [60, 60, 60],
+    [20, 35, 20]
+]
 
-x_input = np.array([15, 25, 35])
-x_input = x_input.reshape((1, 3, 1))
-test_input = torch.tensor(x_input)
-test_input = test_input.type(torch.FloatTensor)
-outputs = net(test_input)
-conf, predicted = torch.max(outputs.data, 1)
-print (predicted[0], conf)
-
-x_input = np.array([45, 70, 80])
-x_input = x_input.reshape((1, 3, 1))
-test_input = torch.tensor(x_input)
-test_input = test_input.type(torch.FloatTensor)
-outputs = net(test_input)
-conf, predicted = torch.max(outputs.data, 1)
-print (predicted[0], conf)
-
-x_input = np.array([60, 60, 60])
-x_input = x_input.reshape((1, 3, 1))
-test_input = torch.tensor(x_input)
-test_input = test_input.type(torch.FloatTensor)
-outputs = net(test_input)
-conf, predicted = torch.max(outputs.data, 1)
-print (predicted[0], conf)
-
-x_input = np.array([20, 35, 20])
-x_input = x_input.reshape((1, 3, 1))
-test_input = torch.tensor(x_input)
-test_input = test_input.type(torch.FloatTensor)
-outputs = net(test_input)
-conf, predicted = torch.max(outputs.data, 1)
-print (predicted[0], conf)
-
+for item in evaluation_set:
+    print(item)
+    x_input = np.array(item)
+    x_input = x_input.reshape((1, 3, 1))
+    test_input = torch.tensor(x_input)
+    test_input = test_input.type(torch.FloatTensor)
+    outputs = net(test_input)
+    conf, predicted = torch.max(outputs.data, 1)
+    print("Predicted class:", int(predicted[0]), "confidence:", float(conf))
